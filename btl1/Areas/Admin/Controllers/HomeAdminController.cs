6784ed1fedc3f909d523de1a1b8f7f253c9aed0d
mowonaaa <117066267+mowonaaa@users.noreply.github.com>
2023-10-28 22:23:20 +0700

    admin

---
+++ btl1/Areas/Admin/Controllers/HomeAdminController.cs
@@ -0,0 +1,25 @@
+﻿using btl1.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace btl1.Areas.Admin.Controllers
+{
+    [Area("admin")]
+    [Route("admin")]
+    [Route("admin/homeadmin")]
+    public class HomeAdminController : Controller
+    {
+        QlbanKhanContext db=new QlbanKhanContext();
+        [Route("")]
+        [Route("index")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [Route("danhmucsanpham")]
+        public IActionResult DanhMucSanPham()
+        {
+            var lstSanPham=db.Sanphams.ToList();
+            return View(lstSanPham);
+        }
+    }
+}
